3d00da1701d781631e7ccf7c30af27787b67d615
oscquitar <orlandooscarqm@gmail.com>
2023-04-13 07:43:04 -0500

    Cambios Archivos html

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 $firstNumber = 2;
 $secondNumber= 4;
 
-echo $firstNumber+$secondNumber;
+echo $firstNumber + $secondNumber;
+echo $firtsNumber - $secondNomber;
+echo $firstNumber *$secondNumber;
+echo $firstNumber / $secondNumber;
 ?>
